126fcc6c87a4e34236980c557f37b59b043869b0
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-06-03 08:55:59 +0530

    port adding

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost:27017/Library');
+mongoose.connect('mongodb://localhost:27017/Library',{ useNewUrlParser: true });
 const Schema = mongoose.Schema;
 
 
