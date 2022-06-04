ef9101c16ae05b2d9dcb1d842f8ddd31979ed1c5
adarsh-2425 <adarsh.lol2425@gmail.com>
2022-06-04 19:59:37 +0530

    updations

--- src/model/BookModel.js
+++ src/model/BookModel.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/Library',{ useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI ||'mongodb://localhost:27017/Library',{ 
+    useNewUrlParser: true,
+    useUnifiedTopology: true });
 const Schema = mongoose.Schema;
 
 
